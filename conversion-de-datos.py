887cd93a87716c59b97eff6d377176e420bb1b86
PAUL VELIZ <paulsystem32@gmail.com>
2021-09-03 00:25:28 -0500

    Python Introduction
    Python Data Types and casting.
    Uadeo Class

---
+++ conversion-de-datos.py
@@ -0,0 +1,27 @@
+# Obtener valores de entrada
+
+num1 = input('Ingresa un numero ')
+num2 = input('Ingresa otro numero ')
+
+# print( type(num1), type(num2) )
+
+
+
+# Castear las cadenas
+
+num1 = int(num1)
+num2 = int(num2)
+
+nums = num1+num2
+
+print(nums)
+
+
+# Decimales a enteros
+
+decimal = 8.34
+entero = int(decimal)
+print(entero)
+
+cadena = str(entero)
+
---
+++ tipos-de-datos.py
@@ -0,0 +1,27 @@
+# formato snake_case
+nombre = 'Paul'
+apellidos = 'Veliz'
+
+# Concatenacion
+nombre_completo = nombre + ' ' + apellidos
+
+
+print('Nombre completo:', nombre, apellidos)
+
+# Multiplicar cadenas
+nombre_x4 = nombre * 4
+print('nombre_x4', nombre_x4)
+
+# Numericos
+entero = 100
+decimal = 34.6
+
+print(type (entero), type(decimal) )
+
+# Booleans
+
+debe_materias = True
+print(type(debe_materias))
+
+if debe_materias:
+    print('El alumno debe materias')
